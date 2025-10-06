0c6ac73745633e8cabce2b6126a1250778556912
melnikovknst <melnikovknst@gmail.com>
2025-10-06 09:34:32 +0700

    optimized

--- lab02/src/stackDepth.ts
+++ lab02/src/stackDepth.ts
@@ -1,25 +1,27 @@
 import { ReversePolishNotationActionDict } from "./rpn.ohm-bundle";
 
+
+function depth(arg1: any, arg2: any) {
+  const L = arg1.stackDepth as StackDepth;
+    const R = arg2.stackDepth as StackDepth;
+    const maxBeforeOp = Math.max(L.max, L.out + R.max);
+    const outBeforeOp = L.out + R.out;
+
+  return { max: maxBeforeOp, out: outBeforeOp - 1 } as StackDepth;
+} 
+
+
 export const rpnStackDepth = {
     Expr(e) {
     return e.stackDepth as StackDepth;
   },
 
   Sum(arg1, arg2, _plus) {
-    const L = arg1.stackDepth as StackDepth;
-    const R = arg2.stackDepth as StackDepth;
-    const maxBeforeOp = Math.max(L.max, L.out + R.max);
-    const outBeforeOp = L.out + R.out;
-
-    return { max: maxBeforeOp, out: outBeforeOp - 1 } as StackDepth;
+    return depth(arg1, arg2) as StackDepth;
   },
 
   Mul(arg1, arg2, _times) {
-    const L = arg1.stackDepth as StackDepth;
-    const R = arg2.stackDepth as StackDepth;
-    const maxBeforeOp = Math.max(L.max, L.out + R.max);
-    const outBeforeOp = L.out + R.out;
-    return { max: maxBeforeOp, out: outBeforeOp - 1 } as StackDepth;
+    return depth(arg1, arg2) as StackDepth;
   },
 
   Stack(s) {
